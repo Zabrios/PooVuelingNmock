651b7a054e37b4ae8a64e6e20faf07bbbcbc11a5
Zabrios <dinocren@gmail.com>
2018-06-25 11:46:01 +0200

    Uploading code

---
+++ PooVuelingNmock.Integration.Tests/CalculadoraTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NMock;
+using NMockTests;
+
+namespace PooVuelingNmock.Integration.Tests
+{
+    public class CalculadoraTests
+    {
+        ICalculadora calc = new Calculadora();
+
+        [DataRow(4, 2, 2)]
+        [DataRow(9, 3, 3)]
+        [DataTestMethod()]
+        public void DivisionTest(int num1, int num2, int resultado)
+        {
+            Assert.IsTrue(iCalculadora.Division(num1, num2) == resultado);
+        }
+
+        [DataRow(4, 2, 8)]
+        [DataRow(6, 3, 18)]
+        [DataTestMethod()]
+        public void MultiplicacionTest(int num1, int num2, int resultado)
+        {
+            Assert.IsTrue(iCalculadora.Multiplicacion(num1, num2) == resultado);
+        }
+
+        [TestMethod()]
+        public void RestaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SumaTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ PooVuelingNmock.Unit.Tests/CalculadoraTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NMock;
+using NMockTests;
+
+namespace PooVuelingNmock.Unit.Tests
+{
+    [TestClass]
+    public class CalculadoraTests
+    {
+        private MockFactory mock;
+        private Mock<ICalculadora> mockObject;
+        //private Calculadora calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            mock = new MockFactory();
+            mockObject = mock.CreateMock<ICalculadora>();
+            //calc = new Calculadora();
+        }
+
+        [TestMethod]
+        public void DivisionTest()
+        {
+            //ICalculadora calc = new ICalculadora<Mockobj;
+            var calc = new Calculadora(mockObject.MockObject);
+            mockObject.Expects.One.MethodWith(x => x.Division(4,2)).WillReturn(2);
+            
+            var result = mockObject.Division(4,2);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod()]
+        public void MultiplicacionTest()
+        {
+            var calc = new Calculadora(mockObject.MockObject);
+            mockObject.Expects.One.MethodWith(x => x.Multiplicacion(4, 2)).WillReturn(8);
+
+            var result = mockObject.Multiplicacion(4, 2);
+            Assert.AreEqual(8, result);
+        }
+
+        [TestMethod()]
+        public void RestaTest()
+        {
+            var calc = new Calculadora(mockObject.MockObject);
+            mockObject.Expects.One.MethodWith(x => x.Resta(4, 2)).WillReturn(2);
+
+            var result = mockObject.Resta(4, 2);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod()]
+        public void SumaTest()
+        {
+            var calc = new Calculadora(mockObject.MockObject);
+            mockObject.Expects.One.MethodWith(x => x.Suma(4, 2)).WillReturn(6);
+
+            var result = mockObject.Suma(4, 2);
+            Assert.AreEqual(6, result);
+        }
+
+    }
+}
